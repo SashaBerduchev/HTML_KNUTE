cdfd780ce7f6ffd53a51ddf27a27e230c1881a87
Санёк Виничук <sashavinichuk@gmail.com>
2022-09-12 23:04:04 +0300

    HTML_KNUTE 1.2

--- js/ScriptLesson 3.js
+++ js/ScriptLesson 3.js
@@ -11,7 +11,25 @@ function IsCorrect(){
     var selvalue = city.value;
     var res = document.getElementById('res');
     console.log(index.value +','+ street.value + ', '+ selvalue +  ', ' + house.value + ', ' + flat.value);
-    let addres = selvalue + ', ' + street.value + ', ' + house.value + ', ' + flat.value + ', ' + index.value;
-    res.value = addres;
+    if(index.value == '' || street.value == '' || city.value == '' || house.value == '' || flat.value == ''){
+        alert("Fields are not filled");
+        return;
+    }else if(/^\d+$/.test(index.value)==false){
+        alert('Index not number');
+        return;
+    }else if(/^\d+$/.test(flat.value)== false){
+        alert('Flat not number');
+        return;
+    }else if(/^\w/.test(street.value)==false){
+        alert("Street must be a string");
+        return;
+    }else{
+        let addres = selvalue + ', ' + street.value + ', ' + house.value + ', ' + flat.value + ', ' + index.value;
+        res.value = addres;
+        alert("Done");
+    }
+
+
+
 
 }
