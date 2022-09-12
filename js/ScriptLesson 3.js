323401127d2a702fcc47b09da7c1d41d65cc1d75
Санёк Виничук <sashavinichuk@gmail.com>
2022-09-12 12:28:49 +0300

    HTML_KNUTE 1.1.1

--- js/ScriptLesson 3.js
+++ js/ScriptLesson 3.js
@@ -9,5 +9,9 @@ function IsCorrect(){
     var house = document.getElementById('house');
     var flat = document.getElementById('flat');
     var selvalue = city.value;
+    var res = document.getElementById('res');
     console.log(index.value +','+ street.value + ', '+ selvalue +  ', ' + house.value + ', ' + flat.value);
+    let addres = selvalue + ', ' + street.value + ', ' + house.value + ', ' + flat.value + ', ' + index.value;
+    res.value = addres;
+
 }
